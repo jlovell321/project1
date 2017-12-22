bd783baa13c864a73165c612191f176cf54b7da0
Nancy M <nancym@Nancys-MacBook-Air.local>
2017-12-21 19:40:40 -0600

    initial files

---
+++ script.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+
+
+
+$(function() {
+        // No query string parameters for this API call.
+        var params = { };
+
+        $.ajax({
+            // NOTE: You must use the same location in your REST call as you used to obtain your subscription keys.
+            //   For example, if you obtained your subscription keys from westcentralus, replace "westus" in the 
+            //   URL below with "westcentralus".
+            url: "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?" + $.param(params),
+            beforeSend: function(xhrObj){
+                // Request headers, also supports "application/octet-stream"
+                xhrObj.setRequestHeader("Content-Type","application/json");
+
+                // NOTE: Replace the "Ocp-Apim-Subscription-Key" value with a valid subscription key.
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","9c92bf494dd64f25bb84d008e2b29bdf");
+            },
+            type: "POST",
+            // Request body
+            data: '{"url": "http://media.breitbart.com/media/2016/12/obama-angry-getty-640x480.jpg"}',
+        }).done(function(data) {
+            // Get face rectangle dimensions
+            var faceRectangle = data[0].faceRectangle;
+            var faceRectangleList = $('#faceRectangle');
+
+            // Append to DOM
+            for (var prop in faceRectangle) {
+                faceRectangleList.append("<li> " + prop + ": " + faceRectangle[prop] + "</li>");
+            }
+
+            // Get emotion confidence scores
+            var scores = data[0].scores;
+            var scoresList = $('#scores');
+
+            // Append to DOM
+            for(var prop in scores) {
+                scoresList.append("<li> " + prop + ": " + scores[prop] + "</li>")
+            }
+        }).fail(function(err) {
+            alert("Error: " + JSON.stringify(err));
+        });
+    });
+
+
+  console.log($("input[name=imgUrl]").val());
+
+
+
+
+
+});
